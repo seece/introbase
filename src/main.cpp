5dd0cecdfa292fa8bffe7befd2becca903acebf5
Pekka Väänänen <pekka.vaananen@iki.fi>
2018-03-16 18:49:45 +0200

    bollen

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@
 	#define BREAK_COMPATIBILITY 0
 #endif
 
-#define TWO_PASS    1
-#define USE_MIPMAPS 1
+#define TWO_PASS    0
+#define USE_MIPMAPS 0
 #define USE_AUDIO   1
 #define NO_UNIFORMS 0
 
@@ -21,9 +21,36 @@
 	#include "debug.h"
 #endif
 
+#define GLSL(version, shader)  "#version " #version "\n" #shader
+
 #include "glext.h"
-#include "shaders/fragment.inl"
+#pragma data_seg(".shader") 
+const char *fragment_frag = R"END(
+#version 430
+#define SCREEN_X (1280/2)
+#define SCREEN_Y (720/2)
+layout(location=0)uniform int ti;
+float t,l,v,f,z,i=0,m=2,n=.3,r=0;
+vec3 y;
+int S=0;
+
+vec3 func(vec2 pos) {
+	float d = length(pos - vec2(1.0) + 0.5*vec2(cos(t), sin(t)));
+	if (d < 0.1) return vec3(1.0);
+	return vec3(0.0);
+}
+
+void main()
+{
+	t=ti/44100.;
+	vec2 fc = gl_FragCoord.xy/vec2(SCREEN_X, SCREEN_Y);
+	vec3 c = func(fc);
+	gl_FragColor=vec4(c, 1.0);
+	};	
+)END";
+//#include "shaders/fragment.inl"
 #if TWO_PASS
+	#pragma data_seg(".shader") 
 	#include "shaders/post.inl"
 #endif
 
@@ -52,15 +79,15 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	SetPixelFormat(hDC, ChoosePixelFormat(hDC, &pfd), &pfd);
 	wglMakeCurrent(hDC, wglCreateContext(hDC));
 	
-	PID_QUALIFIER int pid = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &fragment);
+	PID_QUALIFIER int pid = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &fragment_frag);
 	#if TWO_PASS
-		PID_QUALIFIER int pi2 = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &post);
+		PID_QUALIFIER int pi2 = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &post_frag);
 	#endif
 
 	#if OPENGL_DEBUG
-		shaderDebug(fragment, FAIL_KILL);
+		shaderDebug(fragment_frag, FAIL_KILL);
 		#if TWO_PASS
-			shaderDebug(post, FAIL_KILL);
+			shaderDebug(post_frag, FAIL_KILL);
 		#endif
 	#endif
 
@@ -104,7 +131,9 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 				#endif
 			#endif
 		#else
+		#if TWO_PASS
 			refreshShaders(pid, pi2);
+	#endif
 			position = track.getTime();
 			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, ((int)(position*44100.0)));
 		#endif
@@ -119,6 +148,7 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 				((PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap"))(GL_TEXTURE_2D);
 			#else
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				// copies from GL_BACK (double buffering is enabled) to currently bound image
 				glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, XRES, YRES, 0);
 			#endif
 			((PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture"))(GL_TEXTURE0);
